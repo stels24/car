1abec3d1d6d7cd23c21e764f8d63e7e733082218
stels24 <166283589+stels24@users.noreply.github.com>
2024-04-25 17:31:32 +0300

    Цикл for.py

--- Цикл for.py
+++ Цикл for.py
@@ -1,7 +1,11 @@
 a = ["BMW", "MB", "LADA", "KIA", "HONDA"]
-for i in a:
-    print("Я езжу на автомобиле марки", i)
 cars_count = 0
 for i in a:
     cars_count += 10
+    print("Я езжу на автомобиле марки", i, cars_count)
+
+a = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+cars_count = 0
+cars_count += 10
+for i in a:
     print("Я езжу на автомобиле марки", i, cars_count)
